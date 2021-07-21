39318b1d0ba4600ce203f3de0388ba2f353ec0a9
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2021-07-21 23:27:32 +0400

    finished Lesson1

--- Lesson1/task_6.py
+++ Lesson1/task_6.py
@@ -2,15 +2,19 @@
 «сетевое программирование», «сокет», «декоратор».
 Проверить кодировку файла по умолчанию. Принудительно открыть файл
 в формате Unicode и вывести его содержимое'''
+
+
+
 import locale
 texts = ['сетевое программирование', 'сокет', 'декоратор']
 with open("test_file.txt", 'w') as write:
     for text in texts:
         write.writelines(f'{text}\n')
 write.close()
-print(locale.getpreferredencoding())
+print(f'Кодировка по умолчанию: {locale.getpreferredencoding()}\n')
 
 with open("test_file.txt", 'r', encoding='utf-8') as read:
     for line in read:
         print(line)
+read.close()
 
