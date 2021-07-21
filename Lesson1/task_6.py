698638088a39d0ed22266c605a47f57ee9d5f8da
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2021-07-21 15:07:55 +0400

    finished task6

---
+++ Lesson1/task_6.py
@@ -0,0 +1,16 @@
+'''Создать текстовый файл test_file.txt, заполнить его тремя строками: 
+«сетевое программирование», «сокет», «декоратор».
+Проверить кодировку файла по умолчанию. Принудительно открыть файл
+в формате Unicode и вывести его содержимое'''
+import locale
+texts = ['сетевое программирование', 'сокет', 'декоратор']
+with open("test_file.txt", 'w') as write:
+    for text in texts:
+        write.writelines(f'{text}\n')
+write.close()
+print(locale.getpreferredencoding())
+
+with open("test_file.txt", 'r', encoding='utf-8') as read:
+    for line in read:
+        print(line)
+
